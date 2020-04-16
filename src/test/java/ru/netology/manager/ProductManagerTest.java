d8dec5242062863e65eeed42a5e7b25195eafa35
Grigoriy Gamarin <gam110191@gmail.com>
2020-04-16 12:59:23 +0300

    setup method added

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -1,5 +1,6 @@
 package ru.netology.manager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
@@ -9,49 +10,55 @@ import ru.netology.repository.ProductRepository;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductManagerTest {
+    private ProductRepository repository = new ProductRepository();
+    ProductManager manager = new ProductManager(repository);
+    private Product item1 = new Book(1, "Harry Potter", 100, "J.K.Rowling");
+    private Product item2 = new Book(2, "The Colour of Magic", 200, "Terry Pratchett");
+    private Product item3 = new Smartphone(3, "3310", 500, "Nokia");
+    private Product item4 = new Smartphone(4, "Galaxy S10", 1000, "Samsung");
 
-    @Test
-    void shouldSearchByAuthor() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        Product[] expected = {new Book(1, "Harry Potter", 100, "J.K.Rowling")};
-        Product[] actual = manager.searchBy("J.K.Rowling");
-        assertArrayEquals(expected, actual);
+    @BeforeEach
+    void setUp() {
+        repository.save(item1);
+        repository.save(item2);
+        repository.save(item3);
+        repository.save(item4);
     }
 
-    @Test
-    void shouldSearchByBookName() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        Product[] expected = {new Book(2, "The Colour of Magic", 200, "Terry Pratchett")};
-        Product[] actual = manager.searchBy("The Colour of Magic");
-        assertArrayEquals(expected, actual);
-    }
 
-    @Test
-    void shouldSearchBySmartphoneName() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        Product[] expected = {new Smartphone(4, "Galaxy S10", 1000, "Samasung")};
-        Product[] actual = manager.searchBy("Galaxy S10");
-        assertArrayEquals(expected, actual);
-    }
+        @Test
+        void shouldSearchByAuthor() {
+            Product[] actual = manager.searchBy("J.K.Rowling");
+            Product[] expected = new Product[]{item1};
+            assertArrayEquals(expected, actual);
+        }
 
-    @Test
-    void shouldSearchByManufacturer() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        Product[] expected = {new Smartphone(3, "3310", 500, "Nokia")};
-        Product[] actual = manager.searchBy("Nokia");
-        assertArrayEquals(expected, actual);
-    }
+        @Test
+        void shouldSearchByBookName () {
+            Product[] actual = manager.searchBy("The Colour of Magic");
+            Product[] expected = new Product[]{item2};
+            assertArrayEquals(expected, actual);
+        }
 
-    @Test
-    void shouldSearchByNameNotExist() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        Product actual = repository.findById(11);
-        assertEquals(null, actual);
-    }
+        @Test
+        void shouldSearchBySmartphoneName () {
+            Product[] actual = manager.searchBy("3310");
+            Product[] expected = new Product[]{item3};
+            assertArrayEquals(expected, actual);
+        }
 
-}
+        @Test
+        void shouldSearchByManufacturer () {
+            Product[] actual = manager.searchBy("Samsung");
+            Product[] expected = new Product[]{item4};
+            assertArrayEquals(expected, actual);
+        }
+
+        @Test
+        void shouldSearchByNameNotExist () {
+            ProductRepository repository = new ProductRepository();
+            ProductManager manager = new ProductManager(repository);
+            Product actual = repository.findById(11);
+            assertNull(actual);
+        }
+    }
